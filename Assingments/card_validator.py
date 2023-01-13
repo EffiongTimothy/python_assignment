e65a25685b40cacc00c2a475de819fcc35d3c192
timothy <effiongtimothy1@gmail.com>
2023-01-13 07:09:08 +0100

    pushing python assignment

---
+++ Assingments/card_validator.py
@@ -0,0 +1,24 @@
+def card_type(firstNumber):
+    first_number = int(str(firstNumber)[0])
+    second_number = int(str(firstNumber)[1])
+    if first_number == 4:
+        print("visa")
+    elif first_number == 5:
+        print("Mastercard")
+    elif first_number == 6 and second_number == 7:
+        print("AmericanExpress")
+    else:
+        print("card is unknown")
+
+
+def length(number_length):
+    length_of_number = len(number_length)
+    if length_of_number < 13 or length_of_number > 16:
+        print("card length is invalid")
+    else:
+        print(length_of_number)
+
+
+user_input = input("enter card number for validity check")
+card_type(user_input)
+length(user_input)
